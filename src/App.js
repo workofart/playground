712d963c9249ef60848e03d58bdeb342777c3889
Henry Pan <hanxiangp@gmail.com>
2019-01-23 20:51:11 -0500

    .

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const TodoList = (items) => {
+  return (
+    <div>
+      {items.length > 0 && items}
+    </div>
+  )
+}
+
+// this.setState({items: <TodoItem id={1} text='text' />});
+
+const TodoItem = (item) => {
+  return (
+    <label id={item.id}>{item.text}</label>
+  )
+}
+
 class App extends Component {
+  state = {
+    items: []
+  };
+
   render() {
+  
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Todo List
+        <br/>
+        <TodoList items={this.state.items} />
+        <input />
+        <br/>
+        <button>Add Todo</button>
       </div>
     );
   }
